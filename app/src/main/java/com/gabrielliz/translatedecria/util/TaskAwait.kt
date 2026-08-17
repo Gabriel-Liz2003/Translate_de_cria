f052a9ffeeb716a577b142283594d5d632fa71d6
Gabriel-Liz <102968582+Gabriel-Liz2003@users.noreply.github.com>
2026-08-17 17:48:26 -0300

    core: bridge ML Kit tasks to coroutines

---
+++ app/src/main/java/com/gabrielliz/translatedecria/util/TaskAwait.kt
@@ -0,0 +1,18 @@
+package com.gabrielliz.translatedecria.util
+
+import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+suspend fun <T> Task<T>.awaitResult(): T = suspendCancellableCoroutine { continuation ->
+    addOnSuccessListener { value ->
+        if (continuation.isActive) continuation.resume(value)
+    }
+    addOnFailureListener { error ->
+        if (continuation.isActive) continuation.resumeWithException(error)
+    }
+    addOnCanceledListener {
+        continuation.cancel()
+    }
+}
